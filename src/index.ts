e36d32e845936ac07b97327a07c4d9472d3319ab
Artem <artemalex1990@gmail.com>
2023-05-23 10:27:42 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req: Request, res: Response) => {
-  let helloMessage = 'Hello Incvubator!!'
+  let helloMessage = 'Hello Incubator.EU!!'
   res.send(helloMessage)
 })
 
